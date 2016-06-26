e4278324e0844197c46db41bf6556eb0d585373c
eux <ixue@163.com>
2016-06-26 16:49:41 +0800

    Build click event logic about showing current player's symbol

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,31 @@ $(function() {
 
 	display();
 	render(3);
+	$(".grid").click(play);
 
 });
+
+function play() {
+	const playerA = $(".player-a");
+	const player = $(".player");
+	const grid = $(".grid");
+	const clickNow = $(this);
+	if (clickNow.html() === "") {
+		if (playerA.hasClass("play-now")) {
+			clickNow.html("o");
+			player.removeClass("play-now").eq(1).addClass("play-now");
+		} else {
+			clickNow.html("x");
+			player.removeClass("play-now").eq(0).addClass("play-now")
+		}
+	}
+}
 
 function display() {
 	const cont = $(".container")
 	cont.append("<h4>Have Fun^</h4>");
-	cont.append("<p>Play A: o</p>");
-	cont.append("<p>Play B: x</p>");
+	cont.append("<p class='player player-a play-now'><span>Player A: O</span></p>");
+	cont.append("<p class='player player-b'><span>Player B: X</span></p>");
 
 }
 
