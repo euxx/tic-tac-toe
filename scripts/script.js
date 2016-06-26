de0b21e5c3c96a5722749faa268287bd4ba6a4f0
eux <ixue@163.com>
2016-06-26 12:29:51 +0800

    Initial files

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+
+$(function() {
+
+	display();
+	render(3);
+
+});
+
+function display() {
+
+}
+
+function render(gridNum) {
+	$(".container").append("<div class='box'></div>");
+	const box = $(".box");
+	let i = 0;
+	for ( ; i < gridNum; i++) {
+		box.append("<div class='row'></div>");
+	}
+	const row = $(".row");
+	i = 0;
+	for ( ; i < gridNum; i++) {
+		row.append("<div class='grid' /*tabindex='0'*/></div>");
+	}
+}
